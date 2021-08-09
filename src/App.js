b009be134c10713784607431538de463a63b1be9
AgustinaBallesteros <ballesterosagustina@hotmail.com>
2021-08-09 14:33:31 -0300

    Actualización NavBar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import logo from './velvet-nav.png';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Components:
+import './components/NavBar';
+import NavBar from './components/NavBar';
+
+class App extends React.Component {
+  render () {
+    return(
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <NavBar/> 
+        </header>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+const NavBar = () =>{
+    return(
+        <nav id="headerNavBar">
+            <ul>
+                <li><a href="#home">Inicio</a></li>
+                <li><a href="#products">Productos</a></li>
+                <li><a href="#aboutus">Nosotros</a></li>
+                <li><a href="#contact">Contacto</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default NavBar;
